966ff0130bbd2f20e3923bbd5846b6dc19204e6d
MKashanButt <kashanbutt566@gmail.com>
2025-07-22 20:43:40 +0500

    added config file

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "name" => env('APP_NAME', 'ARCHphp'),
+    "env" => env('APP_ENV', 'local'),
+    "debug" => env('APP_DEBUG', false),
+    "url" => env('DB_URL', '127.0.0.1'),
+    "timezone" => env('APP_TIMEZONE', 'UTC'),
+    "locale" => env('APP_LOCALE', 'en'),
+    "fallback_locale" => env('APP_FALLBACK_LOCALE', 'en'),
+];
